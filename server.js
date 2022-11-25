a057ea7022d35dd8798a93d3a206c0d5cbe238e1
Pravin Rathod <pravin3.rathod@ril.com>
2022-11-25 21:45:11 +0530

    update commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// App Server
 const app = express();
 app.get('/', (req, res) => {
   res.send(`Hello World Response from ${process.pid}`);
